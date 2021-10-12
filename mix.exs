d715bec8b027559d542e4d06f98c916ae91b6115
Daniel Kirch <daniel.kirch@2trde.com>
2021-10-12 14:07:23 +0200

    Fix warnings

--- mix.exs
+++ mix.exs
@@ -20,23 +20,14 @@ defmodule GettextExtractVue.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:poison, ">= 3.0.0"}, {:meck, "~> 0.8"}, {:gettext, "~> 0.16"}]
+    [
+      {:gettext, "~> 0.16"},
+      {:meck, "~> 0.8"},
+      {:poison, "~>4.0"}
+    ]
   end
 end
